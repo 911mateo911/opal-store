54b55c8fd8db6efe4ec337f2c1794718dd1ba772
911mateo911 <malajmateo@gmail.com>
2023-09-14 23:10:48 +0200

    feat(publish-page): fix layout publish page

--- src/app/postim/publiko/_components/PublishForm.tsx
+++ src/app/postim/publiko/_components/PublishForm.tsx
@@ -1,21 +1,37 @@
 'use client';
 
+import clsx from "clsx";
 import { TextInput } from "emeralb/app/_shared/atoms/TextInput";
+import { font_Inter, font_RedHatDisplay } from "emeralb/app/_shared/fonts";
 
 export const PublishForm = () => {
   return (
     <div className="pt-2 px-[10px]" >
-      <h2>
+      <h2 className={clsx(
+        "text-xl mb-2.5 mt-2 tracking-wide font-bold",
+        font_RedHatDisplay.className
+      )} >
         Produkti juaj?
       </h2>
-      <h6>
+      <h6 className={clsx(
+        "text-base mb-1 font-semibold text-grey-80",
+        font_Inter.className
+      )} >
         Titulli
       </h6>
+      <p className={clsx(
+        "text-xs mb-0.5 font-normal text-grey-70",
+        font_Inter.className
+      )} >
+        Nje titull i shkurter dhe permbledhes mbi produktin tend.
+      </p>
       <TextInput
         value="mateo"
         name="text"
         onChange={console.log}
       />
+      <div className="pt-2 grid grid-cols-3" >
+      </div>
     </div>
   )
 }
--- src/app/postim/publiko/layout.tsx
+++ src/app/postim/publiko/layout.tsx
@@ -8,7 +8,7 @@ export default function PublishPageLayout(
   return (
     <div className="max-w-[1000px] mx-auto px-4 my-6 flex justify-center relative" >
       <PublishFormSidebar />
-      <div className='w-full bg-grey-1' >
+      <div className='w-full bg-grey-1 rounded-lg' >
         <PublishFormHeader />
         {children}
       </div>
