d89f88423c706c6636d2cc678b3ba9c77204b76a
911mateo911 <malajmateo@gmail.com>
2023-11-28 15:03:27 +0100

    add lucia auth

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,3 +1,16 @@
+/// <reference types="lucia" />
+
+declare namespace Lucia {
+  type Auth = import('./app/auth/lucia').Auth;
+  type DatabaseUserAttributes = {
+    fullName: string;
+    created_at: string;
+    draftProductId?: string;
+    draftProducts: any[]
+  };
+  type DatabaseSessionAttributes = {};
+}
+
 declare module "*.svg" {
   const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default content;
